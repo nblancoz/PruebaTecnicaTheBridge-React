868c2e59abc43c656ae3ba337518e5ebeef0db89
nblancoz <nblancozappala@gmail.com>
2023-12-23 14:09:51 +0100

    footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <p className="footer">
+        Prueba Técnica realizada en React por: Nicolás Blanco Zappalá
+      </p>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Products from "../Products/Products";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Products />
+      <Footer />
     </>
   );
 };
