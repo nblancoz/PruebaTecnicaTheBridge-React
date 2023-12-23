8bf57b854bf47c13cae9b5f85710493b4e93de07
nblancoz <nblancozappala@gmail.com>
2023-12-23 12:00:51 +0100

    first commit - components created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "../Products/Products";
+
+const Home = () => {
+  return (
+    <>
+      <p>Este es el Home</p>
+      <Products />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+        <p>Aqui va la tabla</p>
+    </div>
+  )
+}
+
+export default Products
